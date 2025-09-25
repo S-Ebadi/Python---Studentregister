45d24a144719b0f33919a8f268b610234e28ed52
S-Ebadi <said.ebadi@chasacademy.se>
2025-09-25 14:33:18 +0200

    Uppdaterat engelsk studentlista

--- English_studentlist.py
+++ English_studentlist.py
@@ -2,7 +2,7 @@ students = []
 
 while True:
     print("\n1. Add student\n2. List students\n3. Find student\n4. Calculate average age\n5. Remove student\n6. Exit")
-    option = input("Choose: ")
+    option = input("Please select option: ")
 
     if option == "1":
         name = input("Name: ")
@@ -35,4 +35,27 @@ while True:
     
     elif option == "4":
         if students:
-            print
+            print("Average age:", sum(s["age"] for s in students)/len(students))
+        else:
+            print("No students registered.")
+        input("Press Enter to return to menu...")
+    
+    elif option == "5":
+        n = input("Name to remove: ")
+        new = [s for s in students if s["name"].lower() != n.lower()]
+        if len(new) < len(students): 
+            students = new
+            print(n, "have been removed.")
+        else:
+            print("Student not found.")
+        input("Press Enter to return to menu...")
+
+    elif option == "6":
+        print("Exiting.")
+        break
+
+    else:
+        print("Invalid choice")
+        input("Press Enter to return to menu...")
+
+    
